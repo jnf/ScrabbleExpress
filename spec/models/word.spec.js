0f08911b96ff88f0b11acb2512e2a60624ad23d1
jnf <jeremy.flores@gmail.com>
2016-06-23 12:37:09 -0700

    update function name to fix test

--- spec/models/word.spec.js
+++ spec/models/word.spec.js
@@ -7,7 +7,7 @@ describe('Word', function () {
 
   describe('.find_by_word', function () {
     it('returns one object from the database', function (done) {
-      Word.find_by_word('elephant', function (error, result) {
+      Word.find_or_create_by_word('elephant', function (error, result) {
         expect(error).toBe(null)
         expect(result).toNotBe(null)
         done()
@@ -15,14 +15,14 @@ describe('Word', function () {
     })
 
     it('returns an object in the right format', function (done) {
-      Word.find_by_word('elephant', function (error, result) {
+      Word.find_or_create_by_word('elephant', function (error, result) {
         expect(Object.keys(result)).toEqual(['id', 'word', 'score'])
         done()
       })
     })
 
     it('return an empty instance of Word when it can not find a word', function(done) {
-      Word.find_by_word("jeremy", function(error, result) {
+      Word.find_or_create_by_word("jeremy", function(error, result) {
         expect(error).toBe(null)
         expect(result).toNotBe(undefined)
         expect(result instanceof Word).toBeTruthy()
