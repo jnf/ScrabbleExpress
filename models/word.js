11f2ddc7bb95e38812e1a1f61c51908260e4e70e
jnf <jeremy.flores@gmail.com>
2016-06-23 10:36:31 -0700

    update .find_by_word to always return an instance of word; update tests to reflect

--- models/word.js
+++ models/word.js
@@ -1,15 +1,26 @@
 var app = require('../app')
 var db = app.get('db')
 
-function Word() {}
+function Word(data) {
+  this.id = data.id
+  this.word = data.word
+  this.score = data.score
+}
 
 // class functions
 Word.find_by_word = function (word, callback) {
-  db.words.findOne({ word: word }, callback)
+  db.words.findOne({ word: word }, function (error, result) {
+    if (error) {
+      callback(error, undefined)
+    } else {
+      // if we have a result, send it to the constructor. else send an empty object
+      callback(null, new Word(result || {}))
+    }
+  })
 }
 
 if(app.get('env') === 'test') {
-  Word.end = function() { db.end() }
+  Word.end = function () { db.end() }
 }
 
 module.exports = Word
--- spec/models/word.spec.js
+++ spec/models/word.spec.js
@@ -8,7 +8,6 @@ describe('Word', function () {
   describe('.find_by_word', function () {
     it('returns one object from the database', function (done) {
       Word.find_by_word('elephant', function (error, result) {
-        console.log(result)
         expect(error).toBe(null)
         expect(result).toNotBe(null)
         done()
@@ -18,6 +17,16 @@ describe('Word', function () {
     it('returns an object in the right format', function (done) {
       Word.find_by_word('elephant', function (error, result) {
         expect(Object.keys(result)).toEqual(['id', 'word', 'score'])
+        done()
+      })
+    })
+
+    it('return an empty instance of Word when it can not find a word', function(done) {
+      Word.find_by_word("jeremy", function(error, result) {
+        expect(error).toBe(null)
+        expect(result).toNotBe(undefined)
+        expect(result instanceof Word).toBeTruthy()
+
         done()
       })
     })
